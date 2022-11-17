bf52992f2df140421f15bf0e441ec3d0f148bfab
gurcan <kadir.guercan@gmail.com>
2022-11-17 16:17:20 +0100

    list

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,35 @@
+class BookmarksController < ApplicationController
+  def new
+    set_list
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    set_list
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    @bookmark.save
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @list_id = @bookmark.list_id
+    @bookmark.destroy
+    redirect_to list_path(@list_id), status: :see_other
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :list_id, :movie_id)
+  end
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get 'lists', to: 'lists#index'
+  get 'lists/new', to: 'lists#new'
+  post 'lists', to: 'lists#create'
+  get 'lists/:id', to: 'lists#show', as: 'list'
+
+  get '/lists/:list_id/bookmarks/new', to: 'bookmarks#new', as: 'new_bookmark'
+  post '/lists/:list_id/bookmarks', to: 'bookmarks#create', as: 'list_bookmarks'
+  delete '/bookmarks/:id', to: 'bookmarks#destroy', as: 'bookmark'
+
 end
